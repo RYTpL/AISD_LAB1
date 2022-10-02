6e736dc16ccdf6400c4f4a3c173c40f3fcaa8c6a
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2022-10-02 22:35:29 +0400

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <malloc.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+#include "matrix.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
---
+++ matrix.h
@@ -0,0 +1,20 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <malloc.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
