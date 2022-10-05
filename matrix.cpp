d046ac60cf40e00428b1d58d641a3198e3e2a3e6
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2022-10-05 21:24:59 +0400

    2 gen

--- matrix.cpp
+++ matrix.cpp
@@ -39,3 +39,103 @@ Matrix::Matrix(int rows, int columns) {
         for (int j = 0; j < columns; j++)
             matrix[i][j] = i+j;
 }
+double& Matrix::operator () (int i, int j) {
+    if ((i >= 0) && (i < rows) && (j >= 0) && (j < columns))
+        return matrix[i][j];
+    else throw "Некорректные значения индексов!";
+}
+Matrix Matrix::operator + (const Matrix& rhs) {
+    if ((rows == rhs.rows) && (columns == rhs.columns)) {
+        Matrix result(rows, columns);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                result.matrix[i][j] = matrix[i][j] + rhs.matrix[i][j];
+        return result;
+    }
+    throw "Размерности матриц не совпадают! Сложение матриц с разными размерностями недопустимо!";
+}
+Matrix Matrix::operator - (const Matrix& rhs) {
+    if ((rows == rhs.rows) && (columns == rhs.columns)) {
+        Matrix result(rows, columns);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                result.matrix[i][j] = matrix[i][j] - rhs.matrix[i][j];
+        return result;
+    }
+    throw "Размерности матриц не совпадают! Вычитание матриц с разными размерностями недопустимо!";
+}
+Matrix Matrix::operator * (const Matrix& rhs) {
+    if (columns == rhs.rows) {
+        Matrix result(rows, rhs.columns);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < rhs.columns; j++)
+                result.matrix[i][j] = 0;
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < rhs.columns; j++)
+                for (int k = 0; k < columns; k++)
+                    result.matrix[i][j] += matrix[i][k] * rhs.matrix[k][j];
+        return result;
+    }
+    throw "Матрицы не могут быть перемножены! Недопустимая размерность!";
+}
+Matrix Matrix::operator * (const double h) {
+    Matrix result(rows, columns);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            result.matrix[i][j] = matrix[i][j] * h;
+    return result;
+}
+Matrix Matrix::operator / (const double h) {
+    Matrix result(rows, columns);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            result.matrix[i][j] = matrix[i][j] / h;
+    return result;
+}
+void Matrix::EnterMatrix() {
+    for (int i = 0; i < rows; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+
+    cout << "Введите размерность матрицы:" << endl;
+    cout << "Количество строк: ";
+    int newRows = int_checker();
+    cout << "Количество столбцов: ";
+    int newColumns = int_checker();
+
+    Matrix newMatrix(newRows, newColumns);
+    for (int i = 0; i < newRows; i++)
+        for (int j = 0; j < newColumns; j++) {
+            cout << "Ячейка (" << i << ", " << j << ") = ";
+            newMatrix.matrix[i][j] = double_checker();
+        }
+
+    rows = newRows;
+    columns = newColumns;
+    matrix = new double* [rows];
+    for (int i = 0; i < rows; i++)
+        matrix[i] = new double[columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            matrix[i][j] = newMatrix.matrix[i][j];
+}
+Matrix operator * (double h, Matrix matrix) {
+    Matrix result(matrix.GetRows(), matrix.GetCols());
+    result = matrix * h;
+    return result;
+}
+Matrix operator / (double h, Matrix matrix) {
+    Matrix result(matrix.GetRows(), matrix.GetCols());
+    result = matrix / h;
+    return result;
+}
+ostream& operator<< (ostream& s, const Matrix& matrix) {
+
+    for (int i = 0; i < matrix.rows; i++) {
+        for (int j = 0; j < matrix.columns; j++)
+            s << matrix.matrix[i][j] << " ";
+        s << "\n";
+    }
+    return s;
+}
--- matrix.h
+++ matrix.h
@@ -27,5 +27,15 @@ private:
 public:
 	Matrix();
 	Matrix(int rows, int columns);
+	void EnterMatrix();
+	double& operator () (int i, int j);
+	Matrix operator + (const Matrix& rhs);
+	Matrix operator - (const Matrix& rhs);
+	Matrix operator * (const Matrix& rhs);
+	Matrix operator * (const double h);
+	Matrix operator / (const double h);
+	friend Matrix operator * (double h, Matrix matrix);
+	friend Matrix operator / (double h, Matrix matrix);
+	friend std::ostream& operator<< (std::ostream& out, const Matrix& matrix);
 
 };
